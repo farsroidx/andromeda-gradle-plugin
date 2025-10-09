b3eaf5bdb12630fdf02daf965c601e4d1598a8a3
Mohammad Ali Riazati <49606584+farsroidx@users.noreply.github.com>
2025-10-09 22:54:24 +0330

    Release: 1.0.0

--- src/main/kotlin/ir/farsroidx/plugin/Version.kt
+++ src/main/kotlin/ir/farsroidx/plugin/Version.kt
@@ -2,5 +2,5 @@ package ir.farsroidx.plugin
 
 object Version {
     const val PLUGIN_VERSION   = "1.0.0"
-    const val RELEASE_DATETIME = 1760037461238
+    const val RELEASE_DATETIME = 1760037725968
 }
